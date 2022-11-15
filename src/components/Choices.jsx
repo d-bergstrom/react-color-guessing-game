134fa5fed7abbdad3303a5bd7ab9579e28e38bfc
David Bergstrom <106361446+david-bergstrom-cohort225@users.noreply.github.com>
2022-11-14 17:16:03 -0800

    Initial commit

---
+++ src/components/Choices.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Choices = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default Choices;
---
+++ src/components/Color.jsx
@@ -0,0 +1,16 @@
+/* 
+Generate Random Color: https://css-tricks.com/snippets/javascript/random-hex-color/
+
+*/
+
+
+import React, { useState } from "react";
+
+
+const Color = () => {
+  return (
+    <div></div>
+  );
+}
+
+export default Color;
