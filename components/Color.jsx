39238d718dc5d350bd44c5d7fb1a6b35efd656ca
David Bergstrom <106361446+david-bergstrom-cohort225@users.noreply.github.com>
2022-11-16 17:33:07 -0800

    Cleaned up comments

--- components/Color.jsx
+++ components/Color.jsx
@@ -1,6 +1,6 @@
 /* 
 Sources Used:
-    - Generate Random Color: https://css-tricks.com/snippets/javascript/random-hex-color/
+    - JavaScript Random Hex Code: https://css-tricks.com/snippets/javascript/random-hex-color/
 */
 
 import React, { useState } from "react";
@@ -9,22 +9,7 @@ import Choices from "./Choices";
 
 const Color = () => {
 
-  //State used for all color choices
-  const [colorChoices, setColorChoices] = useState(fillColorChoices);
-  console.log("Color Choices: ", colorChoices);
-
-  //Pick a random color from the colorChoices state as the answer
-  const answer = colorChoices[Math.floor(Math.random() * 3)];
-  console.log("Answer: ", answer);
-
-  //Object used for inline styling display of the color answer
-  const colorArea = {
-    backgroundColor: `${answer}`,
-    width: "500px",
-    height: "400px",
-  };
-
-  //Function used to generate random hex colors
+  //Function used to generate random hex colors. Passed to child components.
   function fillColorChoices() {
     const numberOfGuesses = 3; //Number of Button Guesses
     const generatedColors = [];
@@ -34,10 +19,20 @@ const Color = () => {
     return generatedColors;
   }
 
-  //Create Child Elements based off how many colorChoices there are
-  const buttonElements = colorChoices.map((color, index) => (
-    <Choices key={index} id={index} color={color} answer={answer} checkAnswer={checkAnswer} />
-  ));
+  //State used for all color choices. Calls fillColorChoices to populate.
+  const [colorChoices, setColorChoices] = useState(fillColorChoices);
+  console.log("Color Choices: ", colorChoices);
+
+  //Pick a random color from the colorChoices state as the answer
+  const answer = colorChoices[Math.floor(Math.random() * 3)];
+  console.log("Answer: ", answer);
+
+  //Object used for inline styling display of the color area using the chosen answer.
+  const colorArea = {
+    backgroundColor: `${answer}`,
+    width: "500px",
+    height: "400px",
+  };
 
   //Function to check if selected answer is correct
   function checkAnswer(selectedColor) {
@@ -46,13 +41,18 @@ const Color = () => {
     setColorChoices(fillColorChoices());
   }
 
+  //Create multiple Child Elements based off how many color choices there are
+  const buttonElements = colorChoices.map((color, index) => (
+    <Choices key={index} color={color} checkAnswer={checkAnswer} />
+  ));
+
+
   return (
     <div>
       {/*Color Box Area*/}
       <div className="colorArea" style={colorArea}></div>
       {/* Buttons */}
       <div className="buttonArea">{buttonElements}</div>
-
     </div>
   );
 }
